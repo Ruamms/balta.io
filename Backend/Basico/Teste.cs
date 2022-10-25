e06ec8622859ba829a075770fb287040dc2f6e98
Ruamms <ruamms3@gmail.com>
2022-10-25 09:38:25 -0300

    Primeira parte

---
+++ Backend/Basico/Teste.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Basico
+{
+    class Teste
+    {
+        static void Main(String[] args)
+        {
+            //int x = 25;
+            //Console.WriteLine(x < 25);
+            if (25 = 25)
+                Console.WriteLine("É igual");
+            else
+                Console.WriteLine("Não é igual");
+        }
+    }
+}
